bd1f268f287c107774d22e2fc8e332b1a82db216
skyle0115 <skyle0115@gmail.com>
2017-05-13 18:12:42 +0800

    redux promise

--- src/reducers/reducer_idea_search.js
+++ src/reducers/reducer_idea_search.js
@@ -1,11 +1,12 @@
 import history from '../history';
 
 export default function (state = null, action) {
+    let next_state;
     switch (action.type) {
         case 'IDEA_SEARCH':
             return action.payload;
         case 'IDEA_LIKE_SEARCH':
-            let next_state = state.slice();
+            next_state = state.slice();
             for (let idea of next_state) {
                 if (idea.i_id === action.payload.i_id) {
                     idea.like_number = action.payload.like_number;
@@ -15,9 +16,17 @@ export default function (state = null, action) {
             }
             return next_state;
         case 'IDEA_COME_UP_WITH':
-            if (action.payload.code === 200) {
-                history.push(`i/${action.payload.i_id}`);
-            }
+            console.log('IDEA_COME_UP_WITH', action);
+            next_state = state.slice();
+            next_state.push({
+                i_id: 123456,
+                idea_type: 'teach',
+                skill: '畫畫',
+                goal: '素描',
+                like_number: 12,
+                liked: true,
+            });
+            return next_state;
         default:
             return state;
     }
